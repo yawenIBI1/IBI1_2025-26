8ce58b8d897a464eb275b9b92371b926e904b576
yawenIBI1 <1760358903@qq.com>
2026-05-13 15:39:31 +0800

    Update generate_random.py

--- Practical 13/generate_random.py
+++ Practical 13/generate_random.py
@@ -1,12 +1,12 @@
 # generate_random.py
 import random
 
-# 20种标准氨基酸列表
+# 20 standard amino acid list
 amino_acids = list("ACDEFGHIKLMNPQRSTVWY")
-# 生成和人/小鼠序列等长（289aa）的随机序列
+# generate equal random sequence between human/mouse（289aa）
 random_protein = "".join(random.choice(amino_acids) for _ in range(289))
 
-# 保存为FASTA格式，和其他文件放在同一个文件夹
+# save as .fasta file
 with open("random_DLX5.fasta", "w") as f:
     f.write(">Random_DLX5_Control (289 aa)\n")
     f.write(random_protein)
